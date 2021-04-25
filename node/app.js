25efb78900c2443b11d3c9b83e26d89d3958ea5b
DevFrog92 <yongsu1481@gmail.com>
2021-04-26 01:48:41 +0900

    [0426] Create chat server

---
+++ node/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const server = app.listen(port, () => {
+  console.log("Express listening on port", port);
+});
+
+const listen = require("socket.io");
+const io = listen(server);
+
+const color = ["yellow", "green", "red", "blue", "white", "black"];
+
+io.on("connection", (socket) => {
+  const username = color[Math.floor(Math.random() * 6)];
+
+  socket.broadcast.emit("join", { username });
+
+  socket.on("client message", (data) => {
+    io.emit("server message", {
+      username,
+      message: data.message,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", { username });
+  });
+});
